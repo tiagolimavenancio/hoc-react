9f1e3c4af3638d07b7ec393b526680093ee8dacb
Tiago Lima <tiagolima@Tiagos-Mac-mini.local>
2019-09-13 13:58:39 -0300

    Applied DismissKeyboard HOC.

---
+++ tackling-hoc-in-react-native/login-hoc/src/HOC/DismissKeyboardHOC.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native'
+
+export default (Comp: ReactClass<*>) => {
+    return ({ children, ...props }: Object) => (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Comp {...props}>
+                { children }
+            </Comp>
+        </TouchableWithoutFeedback>
+    )
+}
